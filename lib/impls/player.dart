ab7734a39f7087dbfe7f14219802111d8d8e4fc2
loyston500 <loyston500@gmail.com>
2022-12-31 10:36:17 +0530

    player json parsing improvement

--- lib/impls/player.dart
+++ lib/impls/player.dart
@@ -11,20 +11,23 @@ class Player extends AudioPlayer {
   factory Player.fromJson(Map<String, dynamic> json) {
     var player = Player()
       ..name = json['name'] as String?
-      ..color = json['color'] as int?;
+      ..color = json['color'] != null
+          ? int.tryParse(json['color'] as String, radix: 16)
+          : null;
 
-    player.setSpeed(json['speed'] as double);
-    player.setUrl(json['url'] as String);
-    player.setVolume(json['volume'] as double);
-    player.setPitch(json['pitch'] as double);
-    player.setLoopMode((json['loop'] as bool) ? LoopMode.one : LoopMode.off);
+    player.setSpeed(json['speed'] as double? ?? 1.0);
+    if (json['url'] != null) player.setUrl(json['url'] as String);
+    player.setVolume(json['volume'] as double? ?? 1.0);
+    player.setPitch(json['pitch'] as double? ?? 1.0);
+    player.setLoopMode(
+        (json['loop'] as bool? ?? false) ? LoopMode.one : LoopMode.off);
 
     return player;
   }
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'name': name,
-        'color': color,
+        'color': color != null ? color!.toRadixString(16) : null,
         'url': url,
         'speed': super.speed,
         'volume': super.volume,
